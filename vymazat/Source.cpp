71ebfb2ac1e9736ca1590453a56dbcf8dbf5b161
martin s <Martinskrlik33@gmail.com>
2021-11-20 10:06:49 +0100

    sd

--- vymazat/Source.cpp
+++ vymazat/Source.cpp
@@ -5,6 +5,7 @@ int main()
 	using namespace std;
 		cout << "We can talk about c++ a while";
 		cout << "you will never lose";
+		cout << "this is the last time I am trying this";
 	cout << endl;
 	cout << "You will never feel sorrow";
 	return 0;
