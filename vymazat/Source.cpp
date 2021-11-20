06237574bcd30d20ae220f0c83d9bca29074bb1c
martin s <Martinskrlik33@gmail.com>
2021-11-20 10:12:03 +0100

    sd

--- vymazat/Source.cpp
+++ vymazat/Source.cpp
@@ -6,6 +6,7 @@ int main()
 		cout << "We can talk about c++ a while";
 		cout << "syou will never lose";
 		cout << "this is the last time I am trying this";
+		cout << "I CANNOT RESOLVE THIS";
 	cout << endl;
 	cout << "You will never feel sorrow";
 	return 0;
